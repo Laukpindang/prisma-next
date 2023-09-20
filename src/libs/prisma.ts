7351f9bde2701b95f41e81ad22618ea8c5ff0677
Laukpindang <ferdiansyahbandung3@gmail.com>
2023-09-20 15:01:25 +0700

    fix prisma

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,5 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+declare global {
+	var prisma: PrismaClient | undefined;
+}
 
-export default prisma
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;

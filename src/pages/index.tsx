b428364d3a750af77a1816a872fbe90cbd7d57a6
Laukpindang <ferdiansyahbandung3@gmail.com>
2023-09-22 14:00:30 +0700

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
-import Head from 'next/head'
-import { useListUser } from '@/hooks/user'
-import { UserResponse } from '@/types/user'
-import { useRouter } from 'next/router'
-import { Card } from '@/components/card'
+/** @format */
 
-export default function Home() {
-  const {data, isLoading, error} = useListUser()
-  const router = useRouter()
-  
+import Head from 'next/head';
+import { useListUser } from '@/hooks/user';
+import { UserResponse } from '@/types/user';
+import { useRouter } from 'next/router';
+import { Card } from '@/components/card';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+
+export async function getServerSideProps() {
+  const user = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const userData = await user.json();
+
+  return {
+    props: { userData },
+  };
+}
+
+export default function Home({
+  userData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const { data, isLoading, error } = useListUser();
+  const router = useRouter();
+
+  console.log(userData);
+
   return (
     <>
       <Head>
@@ -17,18 +33,27 @@ export default function Home() {
       <main className='flex flex-col px-8 py-10 gap-2'>
         <div className='flex flex-row justify-between items-center'>
           <h1 className='font-bold text-xl'>List of Users</h1>
-          <h1 onClick={() => router.push('/add')} className='font-semibold cursor-pointer underline'>Add more</h1>
+          <h1
+            onClick={() => router.push('/add')}
+            className='font-semibold cursor-pointer underline'
+          >
+            Add more
+          </h1>
         </div>
         {isLoading && <span>Loading....</span>}
         {error && <span>Error has occured</span>}
         {data && (
           <div className='grid grid-cols-1: md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4'>
             {data.map((item: UserResponse) => (
-              <Card key={item.id} data={item} onClick={() => router.push(`/detail/${item.id}`)} />
+              <Card
+                key={item.id}
+                data={item}
+                onClick={() => router.push(`/detail/${item.id}`)}
+              />
             ))}
           </div>
         )}
       </main>
     </>
-  )
+  );
 }
